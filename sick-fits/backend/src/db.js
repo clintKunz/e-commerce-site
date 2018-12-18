4faa9923ed5874bfd46648be124f219fd0245570
Clint Kunz <clintkunz@gmail.com>
2018-12-18 11:40:48 -0700

    create graphql yoga server

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,12 @@
+const { Prisma } = require('prisma-binding');
+
+//this file connects to remote prisma db and gives us the ability to query it with JS
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false
+});
+
+module.exports = db; 
