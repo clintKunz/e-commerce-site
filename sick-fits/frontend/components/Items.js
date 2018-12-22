b2c13697a3939aef611bf87a9b03732c0401d092
Clint Kunz <clintkunz@gmail.com>
2018-12-22 16:37:07 -0700

    add links and query for pagination buttons

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -34,7 +34,7 @@ class Items extends Component {
     render() {
         return (
             <Center>
-                <Pagination />
+                <Pagination page={this.props.page} />
                 <Query query={ALL_ITEMS_QUERY}>
                     {({ data, error, loading }) => {
                         if(loading) return <p>Loading...</p>
@@ -44,7 +44,7 @@ class Items extends Component {
                         </ItemsList>;
                     }}
                 </Query>
-                <Pagination />
+                <Pagination page={this.props.page} />
             </Center>
         );
     }
