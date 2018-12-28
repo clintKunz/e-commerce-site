a47f6a8f97da8802b58091d5a1fc668657c11797
Clint Kunz <clintkunz@gmail.com>
2018-12-28 12:25:35 -0700

    add signin mutation to backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -58,6 +58,28 @@ const Mutations = {
         //return user to browser
         return user; 
     },
+
+    async signin(parent, {email, password}, ctx, info) {
+        //check if there is an user with that email
+        const user = await ctx.db.query.user({ where: { email }});
+        if(!user) {
+            throw new Error(`No such user found for email ${email}`);
+        }
+        //check if password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid) {
+            throw new Error(`Invalid password!`);
+        }
+        //generate the jwt token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        //set the cookie with the token 
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        });
+        
+        return user; 
+    }
 };
 
 module.exports = Mutations;
