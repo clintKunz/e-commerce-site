f9c8d8700c3a6bac4a95f53b8d7539f7789c7a15
Clint Kunz <clintkunz@gmail.com>
2018-12-28 11:54:33 -0700

    add middleware to add userId on each request

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,5 +1,6 @@
 // let's go!
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env'})
 const createServer = require('./createServer');
 const db = require('./db');
@@ -9,6 +10,18 @@ const server = createServer();
 // TODO Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 // TODO Use epxress middleware to populate current user
+// decode the JWT to get user ID on each request
+server.express.use((req, res, next) => {
+    const { token } = req.cookies;
+    if(token) {
+        //the APP Secret makes it so that someone can't just add an admin characteristic to the cookie
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        //put userId on the request
+        req.userId = userId;
+    }
+
+    next();
+})
 
 server.start({
     cors: {
